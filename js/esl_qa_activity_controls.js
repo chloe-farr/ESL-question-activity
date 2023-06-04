3a5ddd00bdd09de1aaf4992851671571cd5b98e8
chloe-farr <chloefarr@uvic.ca>
2023-06-04 12:30:27 -0700

    Added a back button and changed the style of the question and answer buttons
    added "progression_button_class" div to contain "go_back_button", "show_answer_button" and "next_question_button".
    show_answer and next_question both only show when appropriate.
    go_back always set to display:block, unless the first question (not answer) is visible, because there's no previous question or answer to show.
    These three buttons are distributed equally vertically, to avoid accidentally double clicking.

--- js/esl_qa_activity_controls.js
+++ js/esl_qa_activity_controls.js
@@ -22,9 +22,8 @@ for (let i = 0; i < buttonElements.length; i++) {
 
 // Initialize variables
 var curGroup = uniqueGroups[0];
-var questionIndex = 0;
+var questionIndex = -1;
 var curQuestion = qa_data[0];
-var questionActive = true;
 
 // Hide question container initially
 questionContainer.style.display = 'none';
@@ -38,7 +37,6 @@ function promptStart() {
   document.getElementById("question_text").innerHTML = "";
   see_answer_button.style.display = 'none';
   next_question_button.style.display = 'none';
-  see_answer_button.addEventListener('click', nextQuestion);
 }
 
 // Function to start the activity
@@ -52,37 +50,48 @@ function startActivity() {
 function changeGroup(group_index) {
   curGroup = uniqueGroups[group_index];
   console.log(curGroup);
-  questionIndex = 0;
+  questionIndex = -1;
   nextQuestion();
   see_answer_button.style.display = 'block';
 }
 
 // Function to proceed to the next question
 function nextQuestion() {
-  showQuestion();
-  questionContainer.style.display = 'block';
+  console.log("qi before incrememt: " + questionIndex);
+  questionIndex++;
+  console.log("qi after increment: " + questionIndex);
   curQuestion = qa_data.find(q => q.group === curGroup && q.question_index === questionIndex.toString());
-
-  if (!curQuestion) {
+  console.log(curQuestion);
+  if (!curQuestion) { //if a question doesn't exist, indicating the end of the question list
     document.getElementById("current_group_name").innerHTML = "";
     promptNextGroup();
   } else {
-    document.getElementById("question_text").innerHTML = curQuestion.question;
-    document.getElementById("current_group_name").innerHTML = `${curGroup} - Question ${curQuestion.question_num}`;
-    document.getElementById("question_image").src = `images/${curQuestion.image}.png`;
+    showQuestion();
   }
+}
+
+function lastAnswer() { 
+  questionIndex--;
+  curQuestion = qa_data.find(q => q.group === curGroup && q.question_index === questionIndex.toString());
+    document.getElementById("current_group_name").innerHTML = `${curGroup} - Question ${curQuestion.question_num}`;
+
+  showAnswer();
 }
 
 // Function to show the question container
 function showQuestion() {
   console.log("show question");
-  if (!questionActive) {
-    questionActive = true;
-    questionContainer.style.display = 'block';
-    answerContainer.style.display = 'none';
-    see_answer_button.style.display = 'block';
-    next_question_button.style.display = 'none';
-  }
+  questionContainer.style.display = 'block';
+  answerContainer.style.display = 'none';
+  see_answer_button.style.display = 'block';
+  next_question_button.style.display = 'none';
+  document.getElementById("question_text").innerHTML = curQuestion.question;
+  document.getElementById("answer_text").innerHTML = "";
+  document.getElementById("current_group_name").innerHTML = `${curGroup} - Question ${curQuestion.question_num}`;
+  document.getElementById("question_image").src = `images/${curQuestion.image}.png`;
+
+  activateGoBackButton();
+  console.log("showing question: " + questionIndex);
 }
 
 // Function to prompt for the next group
@@ -95,18 +104,49 @@ function promptNextGroup() {
 
 // Function to show the answer container
 function showAnswer() {
-  if (questionActive) {
-    questionActive = false;
-    questionContainer.style.display = 'none';
-    answerContainer.style.display = 'block';
-    see_answer_button.style.display = 'none';
-    next_question_button.style.display = 'block';
-  }
+  questionContainer.style.display = 'none';
+  answerContainer.style.display = 'block';
+  see_answer_button.style.display = 'none';
+  next_question_button.style.display = 'block';
+  document.getElementById("question_text").innerHTML = "";
   document.getElementById("answer_text").innerHTML = curQuestion.answer;
-  questionIndex++;
+  activateGoBackButton();
+  console.log("showing answer to: " + questionIndex);
+}
+
+function activateGoBackButton(){
+  first_q_in_group = qa_data.find(q => q.group === curGroup && q.question_index === "0");
+    console.log(first_q_in_group.question);
+
+  if((document.getElementById("question_text").innerHTML === first_q_in_group.question)){
+    go_back_button.style.display = 'none';
+  } else{
+    go_back_button.style.display = 'block';
+  }
+}
+
+// Function to show the answer container
+function goBack() {
+  /*GO TO PREVIOUS ANSWER
+  if the "next question" button was clicked, likely by accident, the following question will show
+  with "go back", we want to show the answer to the previous question.
+  */
+  if (see_answer_button.style.display === 'block') { //ie the question is showing
+    lastAnswer();
+  } 
+  /* SHOW THE ANSWER'S QUESTION
+  if the "show answer" button was clicked, likely by accident, the answer to a question will show.
+  //with "go_back", we want to show the question for that answer. This requires changing the qtext and qimage.
+  */
+  else{ //ie the answer is showing
+    
+    showQuestion();
+  }
+  console.log("went back to QI: " + questionIndex);
 }
 
 // Event listeners for buttons
 see_answer_button.addEventListener('click', showAnswer);
 next_question_button.addEventListener('click', nextQuestion);
+go_back_button.addEventListener('click', goBack);
 
